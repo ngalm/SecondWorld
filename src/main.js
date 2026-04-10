c13e4903be7e6c244a1e7b737892ba9c51f6f831
natalia alm <talia.alm@icloud.com>
2026-04-10 14:31:09 -0700

    added fixed rigid body and trimesh collider for island in physics world

--- src/main.js
+++ src/main.js
@@ -3,140 +3,196 @@ import { PointerLockControls } from 'three/addons/controls/PointerLockControls.j
 import { Water } from 'three/addons/objects/Water.js';
 import { Sky } from 'three/addons/objects/Sky.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import RAPIER from '@dimforge/rapier3d-compat';
 
-// THREE SETUP: loader, scene, camera, and renderer
-const gltfLoader = new GLTFLoader();
-const textureLoader = new THREE.TextureLoader();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(2, 7, 15);      // pov: straight on like person walking on path
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-// render settings for sun in SKY
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.5;
-document.body.appendChild( renderer.domElement );   // append canvas to DOM for renderer to draw to
+async function init() {
+  // THREE SETUP: loader, scene, camera, and renderer
+  const gltfLoader = new GLTFLoader();
+  const textureLoader = new THREE.TextureLoader();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.set(2, 7, 15);      // pov: straight on like person walking on path
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  // render settings for sun in SKY
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 0.5;
+  document.body.appendChild( renderer.domElement );   // append canvas to DOM for renderer to draw to
 
-// SOUND
-const soundPath = './sounds/ambient_ocean.mp3';
-const listener = new THREE.AudioListener();           // create an AudioListener and add it to the camera
-camera.add( listener );                         
-const sound = new THREE.Audio( listener );            // create a global audio source
-const audioLoader = new THREE.AudioLoader();          // load a sound and set it as the Audio object's buffer
-audioLoader.load( soundPath, function( buffer ) {
-	sound.setBuffer(buffer);
-	sound.setLoop(true);
-	sound.setVolume(0.1);
-});
+  // RAPIER SETUP
+  await RAPIER.init();  // asynchronously loaded
+  const gravity = { x: 0.0, y: -9.81, z: 0.0 };
+  const world = new RAPIER.World(gravity);
 
-// CONTROLS
-// Pointer Lock Camera Controls
-const controls = new PointerLockControls( camera, document.body );
-const keys = {}
-document.addEventListener('keydown', event => keys[event.code] = true);     // if a key is pressed it's flagged 'true' in keys object
-document.addEventListener('keyup', event => keys[event.code] = false);      // if a key is not pressed it's flagged 'false' in keys object 
+  // SOUND
+  const soundPath = './sounds/ambient_ocean.mp3';
+  const listener = new THREE.AudioListener();           // create an AudioListener and add it to the camera
+  camera.add( listener );                         
+  const sound = new THREE.Audio( listener );            // create a global audio source
+  const audioLoader = new THREE.AudioLoader();          // load a sound and set it as the Audio object's buffer
+  audioLoader.load( soundPath, function( buffer ) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.1);
+  });
 
-const blocker = document.getElementById( 'blocker' );
-const instructions = document.getElementById( 'instructions' );
+  // CONTROLS
+  // Pointer Lock Camera Controls
+  const controls = new PointerLockControls( camera, document.body );
+  const keys = {}
+  document.addEventListener('keydown', event => keys[event.code] = true);     // if a key is pressed it's flagged 'true' in keys object
+  document.addEventListener('keyup', event => keys[event.code] = false);      // if a key is not pressed it's flagged 'false' in keys object 
 
-instructions.addEventListener( 'click', function () {
-  controls.lock();                      // when user clicks inside 'instructions' html element, pointer is locked (camera controls are active)
-  if (!(sound.isPlaying)) sound.play(); // play sound once user interacts by clicking
-} );
+  const blocker = document.getElementById( 'blocker' );
+  const instructions = document.getElementById( 'instructions' );
 
-controls.addEventListener( 'lock', function () {
-  blocker.style.display = 'none';       // don't display block overlay or instructions menu (since child node of blocker), when pointer is locked (controls are active)
-} );
+  instructions.addEventListener( 'click', function () {
+    controls.lock();                      // when user clicks inside 'instructions' html element, pointer is locked (camera controls are active)
+    if (!(sound.isPlaying)) sound.play(); // play sound once user interacts by clicking
+  } );
 
-controls.addEventListener( 'unlock', function () {
-  blocker.style.display = 'block';       // display instructions menu and blocker overlay when pointer is unlocked (controls deactivated)
-  instructions.style.display = '';
-} );
+  controls.addEventListener( 'lock', function () {
+    blocker.style.display = 'none';       // don't display block overlay or instructions menu (since child node of blocker), when pointer is locked (controls are active)
+  } );
 
-// ISLAND 
-const islandTexturePath = './assets/sand.jpg';
-//const islandNormalsPath = 'assets/islandnormals.jpg';   // for now there is no normal map for the island material, looks better
-const groundTexture = textureLoader.load(islandTexturePath);
-renderer.outputColorSpace = THREE.SRGBColorSpace;         // idk what this does, doesn't appear to do anything to model's render
-//const normalMap = textureLoader.load(islandNormalsPath);
-const material = new THREE.MeshStandardMaterial({
-    map: groundTexture,
-    //normalMap: normalMap,
-    roughness: 0.7,
-});
-const islandPath = './assets/large_island.glb';
-gltfLoader.load(islandPath, 
-  (gltf) => {
-    const model = gltf.scene;
-    model.scale.set(1, 1, 1); 
-    // Traverse the model to find meshes
-    model.traverse((child) => {
-        if (child.isMesh) {
+  controls.addEventListener( 'unlock', function () {
+    blocker.style.display = 'block';       // display instructions menu and blocker overlay when pointer is unlocked (controls deactivated)
+    instructions.style.display = '';
+  } );
+
+  // ISLAND Threejs Mesh
+  const islandTexturePath = './assets/sand.jpg';
+  //const islandNormalsPath = 'assets/islandnormals.jpg';   // for now there is no normal map for the island material, looks better
+  const groundTexture = textureLoader.load(islandTexturePath);
+  renderer.outputColorSpace = THREE.SRGBColorSpace;         // idk what this does, doesn't appear to do anything to model's render
+  //const normalMap = textureLoader.load(islandNormalsPath);
+  const material = new THREE.MeshStandardMaterial({
+      map: groundTexture,
+      //normalMap: normalMap,
+      roughness: 0.7,
+  });
+  const islandPath = './assets/large_island.glb';
+  gltfLoader.load(islandPath, 
+    (gltf) => {
+      const model = gltf.scene;
+      // vars for ISLAND trimesh collider
+      let allVertices = [];
+      let allIndices = [];
+      let indexOffset = 0;
+
+      // Traverse the model to find meshes
+      model.traverse((child) => {
+          if (child.isMesh) {
+            /***DEBUGGING***/
+            console.log("LOCAL scale:", child.scale);
+            console.log("WORLD scale:", child.getWorldScale(new THREE.Vector3()));
+            /***DEBUGGING***/
+
             child.material = material;
             // Optionally, ensure the mesh casts/receives shadows
             child.castShadow = true;
             child.receiveShadow = true;
-        }
-    });
-    scene.add(model);
-    //Start render loop after model loads for smoother appearance when user visits site
-    renderer.setAnimationLoop( animate );
-});
 
-// LIGHT
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
+            // collect vertices and indices for ISLAND Rapier trimesh 
+            child.updateWorldMatrix(true, false);
+            const geometry = child.geometry.clone();
+            geometry.applyMatrix4(child.matrixWorld);
 
-const sunLight = new THREE.DirectionalLight(0xffffff, 1);
-sunLight.position.set(50, 100, 50);
-scene.add(sunLight);
+            /***DEBUGGING***/
+            const debugMesh = new THREE.Mesh(
+              geometry,
+              new THREE.MeshBasicMaterial({
+                wireframe: true,
+                color: 0xff0000
+              })
+            );
+            scene.add(debugMesh);
+            /***DEBUGGING***/
 
-// SKY
-const sky = new Sky(); 
-sky.scale.setScalar( 450000 );
-scene.add( sky );
+            const pos = geometry.attributes.position.array;
+            const idx = geometry.index.array;
+            // add vertices
+            for (let i = 0; i < pos.length; i++) {
+              allVertices.push(pos[i]);
+            }
+            // add indices (offset required!)
+            for (let i = 0; i < idx.length; i++) {
+              allIndices.push(idx[i] + indexOffset);
+            }
+            indexOffset += pos.length / 3;
+          }
+      });
+      // ISLAND Rapier Object setup
+      // create fixed rigid body
+      const islandRigidBodyType = RAPIER.RigidBodyDesc.fixed();
+      const islandRigidBody = world.createRigidBody(islandRigidBodyType);
+      // create trimesh collider
+      const islandColliderType = RAPIER.ColliderDesc.trimesh(new Float32Array(allVertices), new Uint32Array(allIndices));
+      world.createCollider(islandColliderType, islandRigidBody);
 
-const sun = new THREE.Vector3();
-const elevation = 3;
-const azimuth = 180;
-const phi = THREE.MathUtils.degToRad( 90 - elevation);
-const theta = THREE.MathUtils.degToRad(azimuth );
+      scene.add(model); // add island mesh to scene
+      //Start render loop after model loads for smoother appearance when user visits site
+      renderer.setAnimationLoop( animate );
+  });
+  
 
-sun.setFromSphericalCoords( 1, phi, theta );
-sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+  // LIGHT
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
 
-// WATER
-const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
-const waterNormalsPath =  './assets/waternormals.jpg';
-const water = new Water(
-  waterGeometry,
-  {
-    textureWidth: 512,
-    textureHeight: 512,
-    waterNormals: textureLoader.load( waterNormalsPath, function ( texture ) {
-      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    } ),
-    sunDirection: new THREE.Vector3(),
-    sunColor: 0xffd8a8,
-    waterColor: 0x1e90ff,
-    distortionScale: 3.7,
-    fog: scene.fog !== undefined
+  const sunLight = new THREE.DirectionalLight(0xffffff, 1);
+  sunLight.position.set(50, 100, 50);
+  scene.add(sunLight);
+
+  // SKY
+  const sky = new Sky(); 
+  sky.scale.setScalar( 450000 );
+  scene.add( sky );
+
+  const sun = new THREE.Vector3();
+  const elevation = 3;
+  const azimuth = 180;
+  const phi = THREE.MathUtils.degToRad( 90 - elevation);
+  const theta = THREE.MathUtils.degToRad(azimuth );
+
+  sun.setFromSphericalCoords( 1, phi, theta );
+  sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+
+  // WATER
+  const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+  const waterNormalsPath =  './assets/waternormals.jpg';
+  const water = new Water(
+    waterGeometry,
+    {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: textureLoader.load( waterNormalsPath, function ( texture ) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      } ),
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffd8a8,
+      waterColor: 0x1e90ff,
+      distortionScale: 3.7,
+      fog: scene.fog !== undefined
+    }
+  );
+  water.rotation.x = - Math.PI / 2;
+  //water.position.y = 0; // raise or lower to improve glitching at border of island
+  scene.add( water );
+
+  // ANIMATE
+  function animate() {
+    // move camera along xz-axis if controls are locked and a WASD key is pressed
+    if (controls.isLocked) {
+      if (keys['KeyW']) controls.moveForward(.1);
+      if (keys['KeyS']) controls.moveForward(-.1);
+      if (keys['KeyA']) controls.moveRight(-.1);
+      if (keys['KeyD']) controls.moveRight(.1);
+    }
+    water.material.uniforms[ 'time' ].value += 1.0 / 360.0;    // make water move
+    renderer.render( scene, camera );
   }
-);
-water.rotation.x = - Math.PI / 2;
-//water.position.y = 0; // raise or lower to improve glitching at border of island
-scene.add( water );
-
-// ANIMATE
-function animate() {
-  // move camera along xz-axis if controls are locked and a WASD key is pressed
-  if (controls.isLocked) {
-    if (keys['KeyW']) controls.moveForward(.1);
-    if (keys['KeyS']) controls.moveForward(-.1);
-    if (keys['KeyA']) controls.moveRight(-.1);
-    if (keys['KeyD']) controls.moveRight(.1);
-  }
-  water.material.uniforms[ 'time' ].value += 1.0 / 360.0;    // make water move
-  renderer.render( scene, camera );
 }
+
+init();
